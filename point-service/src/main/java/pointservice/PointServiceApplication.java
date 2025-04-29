a6016ceebd5b3ef380a39672215630545a37773b
qheog <qheogus55@naver.com>
2025-04-29 23:56:58 +0900

    [#41]feat: message relay모듈 conponent scan 허용 코드 구현

--- point-service/src/main/java/pointservice/PointServiceApplication.java
+++ point-service/src/main/java/pointservice/PointServiceApplication.java
@@ -2,11 +2,17 @@ package pointservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"pointservice", "outboxmessagerelay"})
+@EntityScan(basePackages = {"pointservice.entity", "outboxmessagerelay.entity"})
+@EnableJpaRepositories(basePackages = {"pointservice.repository", "outboxmessagerelay.repository"})
 @SpringBootApplication
 public class PointServiceApplication {
 
