33d2b8dcff1e2a312f2f1d5dd7caea5a615278b8
qheog <qheogus55@naver.com>
2025-04-29 23:55:32 +0900

    [#41]feat: order-service transactional outbox 패턴 적용

--- order-service/src/main/java/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/orderservice/OrderServiceApplication.java
@@ -2,18 +2,28 @@ package orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableScheduling
+@ComponentScan(basePackages = {"orderservice", "outboxmessagerelay"})
+@EntityScan(basePackages = {"orderservice.entity", "outboxmessagerelay.entity"})
+@EnableJpaRepositories(basePackages = {"orderservice.repository", "outboxmessagerelay.repository"})
 @SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 }
+
+
