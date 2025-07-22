8af88de28900dce8981f62dc913fc8477072b1f0
InHo <qheogus55@naver.com>
2025-07-23 04:14:32 +0900

    [#51]feat: 포인트 서비스 보상 트랜잭션 컨슈머 구현

---
+++ point-service/src/main/java/pointservice/common/config/EventPublisherConfig.java
@@ -0,0 +1,28 @@
+package pointservice.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import event.publisher.EventPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import outboxmessagerelay.OutboxEventPublisher;
+
+@Configuration
+@RequiredArgsConstructor
+public class EventPublisherConfig {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public EventPublisher eventPublisher(KafkaTemplate<String, String> kafkaTemplate) {
+        return new EventPublisher(kafkaTemplate);
+    }
+
+    @Bean
+    public OutboxEventPublisher outboxEventPublisher(){
+        return new OutboxEventPublisher(applicationEventPublisher,objectMapper);
+    }
+}
