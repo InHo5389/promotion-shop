affbe9444abacdaf70e6aaf6f02e18195851221c
InHo <qheogus55@naver.com>
2026-01-03 22:19:06 +0900

    [#65]feat: event pulish 로직 변경 수정

--- common/event/src/main/java/event/EventType.java
+++ common/event/src/main/java/event/EventType.java
@@ -1,10 +1,6 @@
 package event;
 
 import event.payload.*;
-import event.payload.compensation.CompensationCompletedPayload;
-import event.payload.compensation.CouponCompensationRequestPayload;
-import event.payload.compensation.PointCompensationRequestPayload;
-import event.payload.compensation.StockCompensationRequestPayload;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,22 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 @RequiredArgsConstructor
 public enum EventType {
-    ORDER_CREATED(OrderCreatedEventPayload.class, Topic.ORDER),
+    ORDER_CONFIRM(OrderConfirmPayload.class, Topic.ORDER_CONFIRM),
+    STOCK_CONFIRM(StockConfirmPayload.class, Topic.STOCK_CONFIRM),
+    COUPON_CONFIRM(CouponConfirmPayload.class, Topic.COUPON_CONFIRM),
+    POINT_CONFIRM(PointConfirmPayload.class, Topic.POINT_CONFIRM),
+    ORDER_COMPLETE(OrderCompletedPayload.class, Topic.ORDER_COMPLETED);
 
-    COUPON_APPLIED(CouponAppliedEventPayload.class, Topic.COUPON_APPLIED),
-    COUPON_CANCELED(CouponCanceledEventPayload.class, Topic.COUPON_CANCELED),
-
-    STOCK_DECREASE(StockDecreasedEventPayload.class, Topic.PRODUCT_STOCK_DECREASE),
-    STOCK_INCREASE(StockIncreasedEventPayload.class, Topic.PRODUCT_STOCK_INCREASE),
-
-    POINT_USED(PointUsedEventPayload.class, Topic.POINT_USED),
-    POINT_CANCELED(PointEarnedEventPayload.class, Topic.POINT_CANCELED),
-    CART_CLEARED(CartClearedEventPayload.class, Topic.CART),
-
-    STOCK_COMPENSATION_REQUEST(StockCompensationRequestPayload.class, Topic.STOCK_COMPENSATION_REQUEST),
-    COUPON_COMPENSATION_REQUEST(CouponCompensationRequestPayload.class, Topic.COUPON_COMPENSATION_REQUEST),
-    POINT_COMPENSATION_REQUEST(PointCompensationRequestPayload.class, Topic.POINT_COMPENSATION_REQUEST),
-    COMPENSATION_COMPLETED(CompensationCompletedPayload.class, Topic.COMPENSATION_COMPLETED);
 
     private final Class<? extends EventPayload> payloadClass;
     private final String topic;
@@ -43,22 +29,10 @@ public enum EventType {
     }
 
     public static class Topic {
-
-        public static final String ORDER = "order";
-        public static final String ORDER_CANCEL = "order_cancel";
-        public static final String PRODUCT_STOCK_INCREASE = "product_stock_increase";
-        public static final String PRODUCT_STOCK_DECREASE = "product_stock_decrease";
-        public static final String COUPON_APPLIED = "coupon_applied";
-        public static final String COUPON_CANCELED = "coupon_cancel";
-        public static final String CART = "cart";
-        public static final String POINT_USED = "point_use";
-        public static final String POINT_CANCELED = "point_cancel";
-        public static final String PAYMENT = "payment";
-
-        // 보상 토픽들
-        public static final String STOCK_COMPENSATION_REQUEST = "stock_compensation_request";
-        public static final String COUPON_COMPENSATION_REQUEST = "coupon_compensation_request";
-        public static final String POINT_COMPENSATION_REQUEST = "point_compensation_request";
-        public static final String COMPENSATION_COMPLETED = "compensation_completed";
+        public static final String ORDER_CONFIRM = "order-confirm";
+        public static final String STOCK_CONFIRM = "stock-confirm";
+        public static final String COUPON_CONFIRM = "coupon-confirm";
+        public static final String POINT_CONFIRM = "point-confirm";
+        public static final String ORDER_COMPLETED = "order-completed";
     }
 }
